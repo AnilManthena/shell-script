844291940b67639d7a336d4eb0a7ba784cc14ffd
AnilKManthena <akmanthena@gmail.com>
2024-04-18 15:06:37 +0530

    hello-world-script

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hi Ramesh, i am fine. How are you?"
+echo "Ramesh:: I am fine too, How is your work?"
+echo "Suresh:: Not bad, thinking to upgrade to DevOps with AWS?"
