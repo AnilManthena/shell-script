68ae19c3c6956b45a4d305b411ef4add21733665
AnilKManthena <akmanthena@gmail.com>
2024-04-18 23:53:55 +0530

    hello-world-script

--- 10-installation.sh
+++ 10-installation.sh
@@ -11,3 +11,21 @@ else
 fi
 
 dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of MySql is ...Failure"
+    exit 1
+else
+    exho "Installation of MySql is .... Sucess"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of GIT is ..Failure"
+    exit 1
+else
+    echo "Installation of GIT is ...Success"
+fi
