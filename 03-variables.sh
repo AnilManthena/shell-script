83da42ac50eae269f947f5516dc48968f8706c37
AnilKManthena <akmanthena@gmail.com>
2024-04-18 15:35:04 +0530

    hello-world-script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+#declaring variable
 PERSON1=Anil  #no space between = and value
 PERSON2=Sunil
 
-
+#reffering variable
 echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, i am fine. How are you?"
 echo "$PERSON1:: I am fine too, How is your work?"
