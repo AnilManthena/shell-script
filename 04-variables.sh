d3a3efd6333e641e65ebda01faf66d696ad8363a
AnilKManthena <akmanthena@gmail.com>
2024-04-18 15:51:49 +0530

    hello-world-script

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring variable
+PERSON1=$1
+PERSON2=$2
+
+#reffering variable
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine. How are you?"
+echo "$PERSON1:: I am fine too, How is your work?"
+echo "$PERSON2:: Not bad, thinking to upgrade to DevOps with AWS?"
