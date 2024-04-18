826941d89dd46caab160c3023f5f5b0ef23cf2be
AnilKManthena <akmanthena@gmail.com>
2024-04-18 15:23:51 +0530

    hello-world-script

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh  #no space between = and value
+PERSON2=Suresh
+
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine. How are you?"
+echo "$PERSON1:: I am fine too, How is your work?"
+echo "$PERSON2:: Not bad, thinking to upgrade to DevOps with AWS?"
