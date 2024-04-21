7cab6f89cc88d77a74c4eaffa4446a6a8bf65c34
AnilKManthena <akmanthena@gmail.com>
2024-04-21 12:10:22 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,8 +3,13 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "exit status: $1"
-    echo "What are you doing: $2"
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 ...FAILURE"
+        exit 1
+    else
+        echo "$2 ...SUCCESS"
+    fi
 }
 if [ $USERID -ne 0 ]
 then
@@ -20,4 +25,4 @@ VALIDATE $? "Installing MySql"
 dnf install git -y
 VALIDATE $? "INstalling GIT"
 
-echo "is script proceeding"
+
