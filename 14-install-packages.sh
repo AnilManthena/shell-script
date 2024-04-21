ce788052f1a34970567c917edde44e9cb2a6da8b
AnilKManthena <akmanthena@gmail.com>
2024-04-21 14:17:13 +0530

    functions

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 if [ $USERID -ne 0 ]
 then 
     echo "Please run this script with root user"
@@ -12,6 +16,12 @@ fi
 for i in $@
 do
     echo "package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then 
+        echo "$1 already installed... SKIPPING"
+    else
+        echo "$1 Not installed... need to install"
 done
 
 
